c9f202daf31c06d3934e87286163f1915c9a491e
Alberto Benavides <bena.87@hotmail.com>
2023-04-16 13:04:24 -0600

    Spanish lang updated

---
+++ resources/lang/es/paginator.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'title' => 'Revisiones',
+
+    'counts' => [
+        'overview' => ':published, :drafts, :revisions',
+        'published' => ':count published|:count publicados',
+        'drafts' => ':count draft|:count borradores',
+        'revisions' => ':count/:max revisiones',
+    ],
+
+    'published' => 'Publicado',
+    'draft' => 'Borrador',
+    'current' => 'Seleccionado',
+];
